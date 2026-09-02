bc8b6e6621452affdf46df8c0a84b36676886aeb
nirmadhni <nirmaramadhani948@gmail.com>
2026-09-02 21:47:33 +0800

    Memahami struktur dasar kode Java & menggunakan semua jenis Komentar

---
+++ day01.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // haii
+
+        /*
+         * nama saya Sri Nirma Ramadhani 
+         * prodi Informatika 
+         */
+
+        /**
+         * saya mahasiswa informatika 
+         * yang sedang belajar java
+         */
+
+        System.out.println("hii, saya Sri Nirma Ramadhani mahasiswa informatika.");
+    }
+}
