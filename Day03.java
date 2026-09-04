47938024a089f3305197c597ac537f79306f5518
nirmadhni <nirmaramadhani948@gmail.com>
2026-09-04 13:27:36 +0800

    Create Day03.java

---
+++ Day03.java
@@ -0,0 +1,22 @@
+public class Day03 {
+    public static void main(String[] args) {
+
+        // Penggunaan \n
+        System.out.println("Escape Sequence \\n");
+        System.out.println("\\n digunakan untuk membuat baris baru.\nContohnya seperti ini.");
+
+        // Penggunaan \t
+        System.out.println("\nEscape Sequence \\t");
+        System.out.println("\\t digunakan untuk memberikan jarak atau tab.\nContoh\t: Seperti ini.");
+
+        // Penggunaan \"
+        System.out.println("\nEscape Sequence \\\"");
+        System.out.println("\\\" digunakan untuk menampilkan tanda petik.");
+        System.out.println("Contoh: \"halo, saya sedang belajar Java\"");
+
+        // Penggunaan \\
+        System.out.println("\nEscape Sequence \\\\");
+        System.out.println("\\\\ digunakan untuk menampilkan tanda backslash.");
+        System.out.println("Contoh: C:\\Users\\nirmadhni");
+    }
+}
