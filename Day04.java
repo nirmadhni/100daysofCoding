96fc5b13cb1360202258700014cba0b42bbeef0f
nirmadhni <nirmaramadhani948@gmail.com>
2026-09-05 08:12:57 +0800

    Create Day04.java

---
+++ Day04.java
@@ -0,0 +1,28 @@
+public class Day04 {
+public static void main(String[] args) {
+
+
+    // byte digunakan untuk menyimpan bilangan bulat kecil
+    byte nilaiByte = 100;
+
+    // short digunakan untuk menyimpan bilangan bulat
+    // yang lebih besar dari byte
+    short nilaiShort = 10000;
+
+    // int digunakan untuk menyimpan bilangan bulat
+    int nilaiInt = 100000;
+
+    // long digunakan untuk menyimpan bilangan bulat
+    // dengan nilai yang sangat besar
+    long nilaiLong = 1000000000L;
+
+    System.out.println("byte  : " + nilaiByte);
+    System.out.println("short : " + nilaiShort);
+    System.out.println("int   : " + nilaiInt);
+    System.out.println("long  : " + nilaiLong);
+}
+
+
+
+}
+
