7160e75a142b78f6ad95ca7901223a714cd234c4
nirmadhni <nirmaramadhani948@gmail.com>
2026-09-06 12:26:21 +0800

    Create Day05.java

---
+++ Day05.java
@@ -0,0 +1,15 @@
+public class Day05 {
+    public static void main(String[] args) {
+
+        // double digunakan untuk menyimpan angka pecahan dengan ketelitian tinggi
+        double tinggi = 156.5;
+        double berat = 50.5;
+
+        // float digunakan untuk menyimpan angka pecahan dan harus menggunakan huruf f
+        float nilai = 89.5f;
+
+        System.out.println("Tinggi badan: " + tinggi);
+        System.out.println("Berat badan: " + berat);
+        System.out.println("Nilai: " + nilai);
+    }
+}
