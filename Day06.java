d0f7101240338ad9cab2cb12b7069cbb96d09ea2
nirmadhni <nirmaramadhani948@gmail.com>
2026-09-07 14:24:34 +0800

    Create Day06.java

---
+++ Day06.java
@@ -0,0 +1,13 @@
+public class Day06 {
+    public static void main(String[] args) {
+
+        // char digunakan untuk menyimpan satu karakter
+        char nilai = 'A';
+
+        // boolean digunakan untuk menyimpan nilai benar atau salah
+        boolean mahasiswaAktif = true;
+
+        System.out.println("Nilai: " + nilai);
+        System.out.println("Mahasiswa aktif: " + mahasiswaAktif);
+    }
+}
