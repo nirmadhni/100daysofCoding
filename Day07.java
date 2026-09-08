78dd312633b0b1df678b68567c716b969a6659f8
nirmadhni <nirmaramadhani948@gmail.com>
2026-09-08 18:02:49 +0800

    Add Day07 class to display student information

---
+++ Day07.java
@@ -0,0 +1,14 @@
+public class Day07 {
+    public static void main(String[] args) {
+
+        // Tipe data String digunakan untuk menyimpan teks
+        String nama = "Sri Nirma Ramadhani";
+        String prodi = "Informatika";
+        String kampus = "Universitas Sulawesi Barat";
+
+        // Menampilkan data String
+        System.out.println("Nama: " + nama);
+        System.out.println("Prodi: " + prodi);
+        System.out.println("Kampus: " + kampus);
+    }
+}
