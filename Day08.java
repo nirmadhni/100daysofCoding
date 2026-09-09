64f2b2adf606f6dc90cd7a43fbd64186e9ae6a2f
nirmadhni <nirmaramadhani948@gmail.com>
2026-09-09 17:08:09 +0800

    Create Day08.java

---
+++ Day08.java
@@ -0,0 +1,20 @@
+public class Day08 {
+    public static void main(String[] args) {
+
+        // Deklarasi: membuat variabel dan menentukan tipe datanya
+        String nama;
+        int umur;
+
+        // Inisialisasi: memberikan nilai awal ke variabel
+        nama = "Nirma";
+        umur = 18;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+
+        // Update: mengubah nilai variabel yang sebelumnya sudah ada
+        umur = 19;
+
+        System.out.println("Umur setelah di-update: " + umur);
+    }
+}
