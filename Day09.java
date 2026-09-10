17246c1b662229b9af080a4c65e4ce94f133c816
nirmadhni <nirmaramadhani948@gmail.com>
2026-09-10 10:59:06 +0800

    Create Day09.java

---
+++ Day09.java
@@ -0,0 +1,16 @@
+public class Day09 {
+    public static void main(String[] args) {
+
+        // final digunakan untuk membuat nilai tetap
+        final double PHI = 3.14;
+
+        // nilai PHI tidak bisa diubah lagi
+        System.out.println("Nilai PHI: " + PHI);
+
+        // contoh konstanta lainnya
+        final int TAHUN_LAHIR = 2008;
+
+        // nilai tahun lahir juga tidak bisa diubah
+        System.out.println("Tahun lahir: " + TAHUN_LAHIR);
+    }
+}
