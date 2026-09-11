2a6b82037b27b77565b566c564103ae7ff564247
nirmadhni <nirmaramadhani948@gmail.com>
2026-09-11 19:54:29 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,16 @@
+public class Day10 {
+    public static void main(String[] args) {
+
+        // batas nilai byte
+        System.out.println("byte: -128 sampai 127");
+
+        // batas nilai short
+        System.out.println("short: -32768 sampai 32767");
+
+        // batas nilai int
+        System.out.println("int: -2147483648 sampai 2147483647");
+
+        // batas nilai long
+        System.out.println("long: -9223372036854775808 sampai 9223372036854775807");
+    }
+}
