e2ef7d296864755424c165603edcaf7959468636
nirmadhni <nirmaramadhani948@gmail.com>
2026-09-11 23:12:45 +0800

    Update Day10.java

--- Day10.java
+++ Day10.java
@@ -1,16 +1,22 @@
 public class Day10 {
     public static void main(String[] args) {
 
-        // batas nilai byte
-        System.out.println("byte: -128 sampai 127");
+        // batas nilai tipe data byte
+        System.out.println("Batas byte: " + Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE);
 
-        // batas nilai short
-        System.out.println("short: -32768 sampai 32767");
+        // batas nilai tipe data short
+        System.out.println("Batas short: " + Short.MIN_VALUE + " sampai " + Short.MAX_VALUE);
 
-        // batas nilai int
-        System.out.println("int: -2147483648 sampai 2147483647");
+        // batas nilai tipe data int
+        System.out.println("Batas int: " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
 
-        // batas nilai long
-        System.out.println("long: -9223372036854775808 sampai 9223372036854775807");
+        // batas nilai tipe data long
+        System.out.println("Batas long: " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
+
+        // batas nilai tipe data float
+        System.out.println("Batas float: " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE);
+
+        // batas nilai tipe data double
+        System.out.println("Batas double: " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
     }
 }
