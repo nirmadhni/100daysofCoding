bb7698eca036662cb5163db2f15b4f0e253e6997
nirmadhni <nirmaramadhani948@gmail.com>
2026-09-15 20:43:22 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,24 @@
+public class Day14 {
+    public static void main(String[] args) {
+
+        // Operator Penjumlahan (+)
+        int angka1 = 20;
+        int angka2 = 10;
+
+        int hasilTambah = angka1 + angka2;
+
+        System.out.println("=== PENJUMLAHAN ===");
+        System.out.println("Angka 1: " + angka1);
+        System.out.println("Angka 2: " + angka2);
+        System.out.println("Hasil Penjumlahan: " + hasilTambah);
+
+
+        // Operator Pengurangan (-)
+        int hasilKurang = angka1 - angka2;
+
+        System.out.println("\n=== PENGURANGAN ===");
+        System.out.println("Angka 1: " + angka1);
+        System.out.println("Angka 2: " + angka2);
+        System.out.println("Hasil Pengurangan: " + hasilKurang);
+    }
+}
