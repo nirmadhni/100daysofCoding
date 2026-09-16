c749a855f0ed8878abaac8670e9b4afe90660c9a
nirmadhni <nirmaramadhani948@gmail.com>
2026-09-16 18:57:39 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,18 @@
+public class Day15 {
+    public static void main(String[] args) {
+
+        // Operator Perkalian (*)
+        int angka1 = 10;
+        int angka2 = 5;
+        int hasilKali = angka1 * angka2;
+
+        System.out.println("Hasil Perkalian: " + hasilKali);
+
+        // Operator Pembagian (/)
+        int angka3 = 20;
+        int angka4 = 4;
+        int hasilBagi = angka3 / angka4;
+
+        System.out.println("Hasil Pembagian: " + hasilBagi);
+    }
+}
