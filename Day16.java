94778d6922b9fa4cd444e92fcc5c02a1a3f3ca2d
nirmadhni <nirmaramadhani948@gmail.com>
2026-09-17 20:38:22 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,14 @@
+public class Day16 {
+    public static void main(String[] args) {
+
+        int angka1 = 10;
+        int angka2 = 3;
+
+        int hasil = angka1 % angka2;
+
+        System.out.println("===== OPERATOR MODULUS =====");
+        System.out.println("Angka pertama: " + angka1);
+        System.out.println("Angka kedua: " + angka2);
+        System.out.println("Sisa bagi: " + hasil);
+    }
+}
